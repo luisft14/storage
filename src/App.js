0a8a2acaba7349e37d8176c37262853b07138947
luisft14 <luisf3462@gmail.com>
2021-10-11 11:16:19 -0500

    conexion storage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import {app} from './Firebase';
 function App() {
+  const [archivoUrl,setArchivoUrl]=useState(null);
+  const archivo= async(e)=>{
+    const archivo = e.target.files[0];
+    const storageRef = app.storage().ref();
+    const archivoPath = storageRef.child(archivo.name);
+    await archivoPath.put(archivo); 
+    console.log("archivo cargado "+archivo.name);
+    const enlaceUrl= await archivoPath.getDownloadURL();
+    setArchivoUrl(enlaceUrl);
+
+  }
+
+  const agregarDatos=async(e)=>{
+    const nombre="image";
+    e.preventDefault();
+    const docRef = app.firestore().collection('imagenes').doc(nombre);
+
+    await docRef.set({
+      imagen:archivoUrl
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={agregarDatos}>
+        <input type="file" onChange={archivo} name=""/>
+        <input type="text" onChange={archivo} name="nombre" placeholder="Nombre"/>
+        <button>Enviar</button>
+      </form>
     </div>
   );
 }
---
+++ src/Firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import "firebase/compat/firestore";
+export const app= firebase.initializeApp({
+  apiKey: "AIzaSyD2O22JW0aL2bvVG2aL5kZ0-SNEqJqN0Y4",
+  authDomain: "crud-react-c4132.firebaseapp.com",
+  projectId: "crud-react-c4132",
+  storageBucket: "crud-react-c4132.appspot.com",
+  messagingSenderId: "1030884035870",
+  appId: "1:1030884035870:web:1fc1463a4bc865f5d94d0f"
+});
